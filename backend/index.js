05f8b5656da2790312e4e6d2274204abf4a33411
Dinesh Jana <dinesh.jana22xt@gmail.com>
2025-01-17 14:15:01 +0530

    Routes and test route created.

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.json({
-        message: "Hello World!"
-    })
-})
+const testRoutes = require('./routes/test.route')
 
+app.use('/api', testRoutes);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
