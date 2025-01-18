01b5b15f092951af3c3499c454f3fe4e06e625ec
Dinesh Jana <dinesh.jana22xt@gmail.com>
2025-01-18 15:15:53 +0530

    Connected to the Database.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose")
+require("dotenv").config()
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URI).then(() => {
+    console.log("Connected to the database");
+}).catch((err) => {
+    console.log("Error to connect to the database", err);
+})
 
 const testRoutes = require('./routes/test.route')
 
